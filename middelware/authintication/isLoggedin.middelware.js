8b698b2a80ee22cf8f92a0513764a0c43f2bf1ab
Abdullah Mohammed Ahmed Basheer <auu1637@alhamranigroup.com>
2022-03-01 15:39:03 +0300

    signin, isloggedin, isadmin, isteacher they are done

---
+++ middelware/authintication/isLoggedin.middelware.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+require('dotenv').config({ path: `${__dirname}/../.env` });
+const SECRET = process.env.SECRET 
+
+module.exports = async ( req, res, next ) => {
+    const token = req.header('user-auth-token')
+
+    if (!token) return res.status(400).json({msg:"no token found"})
+    console.log(1)
+    try{
+        let user =  await jwt.verify(token,SECRET)
+        res.locals.user=user
+        next()
+    }catch(e){
+        return res.status(400).json({msg:"Invalid Token",err:e})
+    }
+}
--- routes/user.route.js
+++ routes/user.route.js
@@ -2,22 +2,26 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
 require('dotenv').config({ path: `${__dirname}/../.env` });
 const db = require('../config/db');
 const signupValidation = require('../middelware/validation/signup.middelware');
+const signinValidation = require('../middelware/validation/signin.middelware');
+const isLoggedin = require('../middelware/authintication/isLoggedin.middelware');
+const isAdmin = require('../middelware/authintication/isAdmin.middelware');
+const isTeacher = require('../middelware/authintication/isTeacher.middelware');
 
 const salt = process.env.SALT || 10
-
+const SECRET = process.env.SECRET 
 router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
 
 
-
-// router.get('/',(req,res)=>{
-//     res.send("hi from user route ")
-// })
+// this route for testing the authentications
+router.get('/test',isLoggedin, isAdmin,(req,res)=>{
+    res.send("hi from user route ")
+})
 
 
 
@@ -25,11 +29,11 @@ router.post('/signup',signupValidation,async(req,res) => {
 
     // console.log("1")
     
-    if ( res.locals.erorrslog ) { res.status(400).json({msg:"validation error", errors:res.locals.erorrslog}) }
+    if ( res.locals.erorrslog ) { return res.status(400).json({msg:"validation error", errors:res.locals.erorrslog}) }
 
     else { 
 
-        if(!res.locals.validatedData) res.status(500).json({msg:"validation internal error"});
+        if(!res.locals.validatedData) return res.status(500).json({msg:"validation internal error"});
         
         // console.log("2")
         // console.log(res.locals.validatedData.password)
@@ -45,6 +49,9 @@ router.post('/signup',signupValidation,async(req,res) => {
             res.locals.validatedData.password=hashedPassword
 
             // console.log(res.locals.validatedData)
+            // let { Fname, Lname, password, gender, email, country, birth_date, language } = req.locals.validatedData
+            // console.log(Fname)
+            // console.log("4")
 
             let Fname      =  res.locals.validatedData.Fname
             let Lname      =  res.locals.validatedData.Lname
@@ -57,14 +64,14 @@ router.post('/signup',signupValidation,async(req,res) => {
 
             let sql= ' INSERT INTO user (Fname,Lname,email,password,gender,country,birth_date,language) VALUES(?,?,?,?,?,?,?,?)'
             let submit = await db.query( sql, [Fname=Fname, Lname=Lname, email=email, password=password, gender, country=country, birth_date=birth_date, language=language] )
-            console.log(submit)
-            res.status(200).json({msg:"user has been created successfuly", data:res.locals.validatedData})
+            // console.log(submit)
+            return res.status(200).json({msg:"user has been created successfuly", data:res.locals.validatedData})
 
         }catch(e){
 
-            if(e.code == "ER_DUP_ENTRY") res.status(404).json({msg:e.message, code:e.code, err_no:e.errno, sql_msg:e.sqlMessage})
+            if(e.code == "ER_DUP_ENTRY") return res.status(404).json({msg:e.message, code:e.code, err_no:e.errno, sql_msg:e.sqlMessage})
 
-            res.status(404).json(e)
+            return res.status(404).json(e)
         }
     }
 })
@@ -72,7 +79,55 @@ router.post('/signup',signupValidation,async(req,res) => {
 
 
 // TODO: signin
-// TODO: sginout
+router.post('/signin',signinValidation, async(req,res) => {
+    console.log('inside signin post request')   
+    if ( res.locals.erorrslog ) { return res.status(400).json({msg:"validation error", errors:res.locals.erorrslog}) }
+    else { 
+        if(!res.locals.validatedSigninData) return res.status(500).json({msg:"validation internal error"});
+        console.log("1")
+        console.log(res.locals.validatedSigninData)
+
+        try{
+            let email = res.locals.validatedSigninData.email
+            let password   = res.locals.validatedSigninData.password
+            // let hashedPassword =  await bcrypt.hash(password,parseInt(salt))
+            let sql= 'SELECT * FROM user WHERE email = ?'
+            let submit = await db.query( sql, email )
+            
+            // console.log(submit[0].length)
+
+            if ( submit[0].length === 0 ) return res.status(404).json({msg:"Wrong email or password"})
+            else{
+                
+                let passIsValid = await bcrypt.compare(password,submit[0][0].password)
+                // console.log(passIsValid)
+                
+                let data = {
+                    user_id: submit[0][0].user_id,
+                    userType: submit[0][0].userType,
+                    teach_status: submit[0][0].teach_status,
+                    Fname: submit[0][0].Fname,
+                    Lname: submit[0][0].Lname,
+                }
+
+                const token = await jwt.sign({data},SECRET,{expiresIn:604800})
+
+                if (passIsValid) return res.status(200).json({msg:"user has been logged in successfuly", token})
+                else return res.status(404).json({msg:"Wrong email or password"})
+            
+            }
+        
+        }catch(e){
+            console.log(e)
+            return res.status(404).json(e)
+        }
+
+    }
+
+})
+
+
+// TODO: sginout -- **this from front end just delete the token
 
 // TODO: ask for teacher account (user)
 // TODO: teacher account request (admin)
